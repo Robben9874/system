34e8c25be30aa1003dc9992f12bbd84995afc129
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-10-26 21:14:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-if (message.content.startsWith("kick")) {
+if (message.content.startsWith("!!kick")) {
     var mention = message.mentions.members.first();
     if(!mention) return message.channel.send("يجب منشن العضو");
 
@@ -72,6 +72,17 @@ if (message.content.startsWith("kick")) {
     
     message.channel.send("تم أعطاء كيك الى : " + mention.tag);
 };
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "chat");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
 });
 
 client.on('message', message => {
